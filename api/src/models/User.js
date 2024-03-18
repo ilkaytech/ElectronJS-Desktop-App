66a9118e191d039f415a67c80426a335fcd7f7da
ilkay <06ilkayozturk@gmail.com>
2024-03-18 22:33:16 +0300

    user model created

---
+++ api/src/models/User.js
@@ -0,0 +1,79 @@
+"use strict";
+/* --------------------------------------
+      POİNT OF SALE ELECTRON PROJECT
+----------------------------------------- */
+const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- *
+{
+    "username": "admin",
+    "password": "aA*123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isAdmin": true
+}
+{
+    "username": "test",
+    "password": "aA*123456",
+    "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
+    "isActive": true,
+    "isAdmin": false
+}
+/* ------------------------------------------------------- */
+// User Model:
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("User", UserSchema);
